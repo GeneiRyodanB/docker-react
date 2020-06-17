0aaf4793ddf5fce59bca8f92db4869f82ae4d75e
GeneiRyodanB <hicham.benmehdi@gmail.com>
2020-06-17 18:19:38 +0200

    update App.js in feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was chanaged in feature branch
+          I was chanaged in feature branch.
         </a>
       </header>
     </div>
